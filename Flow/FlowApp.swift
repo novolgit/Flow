dbaf3d5b8820ed26aab849483d2c71037bf190f5
novolgit <groundfor@mail.ru>
2020-12-30 16:06:31 +0300

    Initial Commit

---
+++ Flow/FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowApp.swift
+//  Flow
+//
+//  Created by Vlad Novol on 30.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
